02010bcd3224b6fc836eba55a55412c991c584bb
Tomaš <tomasl.bpti@gmail.com>
2020-08-28 13:12:01 +0300

    unit testing onSubmit

--- Project_Dashboard/angular-dashboard/src/app/widget-form-management/widget-form.component.spec.ts
+++ Project_Dashboard/angular-dashboard/src/app/widget-form-management/widget-form.component.spec.ts
@@ -4,7 +4,7 @@ import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { WidgetFormComponent } from './widget-form.component';
 import { Store } from '@ngrx/store';
 import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
-import { Subject, throwError, of } from 'rxjs';
+import { Subject } from 'rxjs';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { Router, ActivatedRoute } from '@angular/router';
 import  * as widgetActionTypes from './store/widget.actions';
@@ -20,13 +20,7 @@ describe('WidgetFormComponent', () => {
   let store$: MockStore<any>;
   let router: Router;
   let activatedRoute: ActivatedRoute;
-  let widgetForm = new FormGroup({
-    title: new FormControl(''),
-    column: new FormControl(null),
-    type: new FormControl(1),
-    headerType: new FormControl(1),
-    data: new FormControl('')
-  });
+  let widgetForm: FormGroup; 
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -55,32 +49,59 @@ describe('WidgetFormComponent', () => {
     activatedRoute = TestBed.inject(ActivatedRoute);
   });
 
-  // describe('getCleanData', () => {
-  //   it('should serialize data to JSON suitable format', () => {
-  //   });
-  // });
-
-
   describe('onSubmit', () => {
+
+
+    it('should convert input data to JSON suitable format', () => {
+      let formToBeSerialized = new FormGroup({
+        title: new FormControl('MyTitle'),
+        column: new FormControl(3),
+        type: new FormControl(1),
+        headerType: new FormControl(2),
+        data: new FormControl("[{'firstName': 'Tom'}]")
+      });
+      component.widgetForm = formToBeSerialized;
+      
+      spyOn(window, 'alert');
+      component.onSubmit();
+      expect(window.alert).not.toHaveBeenCalled();
+    });
+
+    it('should alert on invalid JSON', () => {
+      component.widgetForm = new FormGroup({
+        title: new FormControl('MyTitle'),
+        column: new FormControl(3),
+        type: new FormControl(1),
+        headerType: new FormControl(2),
+        data: new FormControl('not json data')
+      });
+      spyOn(window, 'alert');
+      component.onSubmit();
+      expect(window.alert).toHaveBeenCalledWith('Data input is not valid JSON.');
+    })
+
+
     it('should navigate when createWidgetSuccess or updateWidgetSuccess to /dashboard', () => {
-      component.widgetForm = widgetForm;
       let testId: number = 5;
-      let widget = {    
-        id : testId,
-        title: 'MyTile',
-        column: 3,
-        type: 1,
-        headerType: 2,
-        data: ''
-      };
+      let widgetValid = {id : testId, title: 'MyTile', column: 3, type: 1, headerType: 2, data: '[]'};
+      let validFormGroup = new FormGroup({
+        title: new FormControl('MyTitle'),
+        column: new FormControl(3),
+        type: new FormControl(1),
+        headerType: new FormControl(2),
+        data: new FormControl('[]')
+      });
+
+      component.widgetForm = validFormGroup;
       const update: Update<widgetInterface> = {
         id: testId,
         changes: {
-          ...widget
+          ...widgetValid
         }
       };
+
       component.onSubmit();
-      actions$.next(...[widgetActionTypes.createWidgetSuccess({widget}), widgetActionTypes.updateWidgetSuccess({update})]);
+      actions$.next(...[widgetActionTypes.createWidgetSuccess({widget: widgetValid}), widgetActionTypes.updateWidgetSuccess({update})]);
       expect(router.navigateByUrl).toHaveBeenCalledWith('/dashboard');
     });
   });
@@ -88,7 +109,6 @@ describe('WidgetFormComponent', () => {
 
   describe('onDelete', () => {
     
-
     it('should dispatch deleteWidget action', () => {
       spyOn(store$, 'dispatch');  
       
